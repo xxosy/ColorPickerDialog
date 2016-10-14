9d782b2339ad09c73e3a860c3416d7f32660bde6
Deok <sso_yaa@naver.com>
2016-10-14 16:48:43 +0900

    sample iing...

--- app/src/main/java/com/testapplication/MainActivity.java
+++ app/src/main/java/com/testapplication/MainActivity.java
@@ -1,27 +1,36 @@
 package com.testapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.FrameLayout;
 
 import com.library.ColorPickerDialog;
-
+import com.library.OnColorPickerButtonClickListener;
 
 
 public class MainActivity extends AppCompatActivity {
     FrameLayout button;
+    FrameLayout background;
     ColorPickerDialog colorPickerDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (FrameLayout)findViewById(R.id.button);
+        background = (FrameLayout)findViewById(R.id.activity_main);
         colorPickerDialog = new ColorPickerDialog(this);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 colorPickerDialog.show();
+                colorPickerDialog.setOnColorPickerButtonClickListener(new OnColorPickerButtonClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        background.setBackgroundColor(Color.parseColor(colorPickerDialog.getColor()));
+                    }
+                });
             }
         });
     }
