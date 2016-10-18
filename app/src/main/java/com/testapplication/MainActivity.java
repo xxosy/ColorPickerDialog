0a9e8f02e9064d8221a074ade1ecac135ae2f80b
Deok <sso_yaa@naver.com>
2016-10-18 17:49:10 +0900

    builder pattern

--- app/src/main/java/com/testapplication/MainActivity.java
+++ app/src/main/java/com/testapplication/MainActivity.java
@@ -11,27 +11,28 @@ import com.library.OnColorPickerButtonClickListener;
 
 
 public class MainActivity extends AppCompatActivity {
-    FrameLayout button;
-    FrameLayout background;
-    ColorPickerDialog colorPickerDialog;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = (FrameLayout)findViewById(R.id.button);
-        background = (FrameLayout)findViewById(R.id.activity_main);
-        colorPickerDialog = new ColorPickerDialog(this);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                colorPickerDialog.show();
-                colorPickerDialog.setOnColorPickerButtonClickListener(new OnColorPickerButtonClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        background.setBackgroundColor(Color.parseColor(colorPickerDialog.getColor()));
-                    }
-                });
-            }
+  FrameLayout button;
+  FrameLayout background;
+  ColorPickerDialog colorPickerDialog;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    button = (FrameLayout) findViewById(R.id.button);
+    background = (FrameLayout) findViewById(R.id.activity_main);
+    colorPickerDialog = new ColorPickerDialog(this);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        colorPickerDialog.show();
+        colorPickerDialog.setOnColorPickerButtonClickListener(new OnColorPickerButtonClickListener() {
+          @Override
+          public void onClick(View view) {
+            background.setBackgroundColor(Color.parseColor(colorPickerDialog.getColor()));
+          }
         });
-    }
+      }
+    });
+  }
 }
